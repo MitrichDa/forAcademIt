5b899bd46ef56a33dd1a0ec9e7012913f9c84c8b
MitrichDa <mitrich.da@gmail.com>
2019-01-31 17:29:00 +0700

    1. Разпрелил классы ко пакетам. 2. Убрал проверку с эпсилон. 3. Убрал использование метода IsInside и get внутри Range, удалил метод isRangeInside. 4. Изменил логику программы при касании отрезков.

---
+++ Range/src/tk/dmitriikorenev/range/classes/Range.java
@@ -0,0 +1,72 @@
+package tk.dmitriikorenev.range.classes;
+
+public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = Math.min(from, to);
+        this.to = Math.max(from, to);
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double x) {
+        return x > from && x < to;
+    }
+
+    private boolean isIntersect(Range range) {
+        return (this.from < range.to && this.to > range.from);
+    }
+
+    public Range getIntersection(Range range) {
+        if (this.isIntersect(range)) {
+            return new Range(Math.max(this.from, range.from), Math.min(this.to, range.to));
+        }
+        return null;
+    }
+
+    public Range[] getDifference(Range range) {
+        if (!this.isIntersect(range)) {
+            return new Range[]{new Range(this.from, this.to)};
+        } else if (range.from > this.from && range.to < this.to) {
+            return new Range[]{new Range(this.from, range.from), new Range(range.to, this.to)};
+        } else if (this.from >= range.from && this.to <= range.to) {
+            return new Range[]{};
+        } else if (range.from > this.from) {
+            return new Range[]{new Range(this.from, range.from)};
+        } else {
+            return new Range[]{new Range(this.to, range.to)};
+        }
+    }
+
+    public Range[] getUnion(Range range) {
+        if (this.isIntersect(range) || this.from == range.to || this.to == range.from) {
+            return new Range[]{new Range(Math.min(this.from, range.from), Math.max(this.to, range.to))};
+        }
+        return new Range[]{new Range(this.from, this.to), new Range(range.from, range.to)};
+    }
+
+    @Override
+    public String toString() {
+        return "(" + from + ", " + to + ")";
+    }
+}
--- Range/src/tk/dmitriikorenev/range/main/RangeRunner.java
+++ Range/src/tk/dmitriikorenev/range/main/RangeRunner.java
@@ -1,11 +1,16 @@
-package tk.dmitriikorenev.range;
+package tk.dmitriikorenev.range.main;
+
+import tk.dmitriikorenev.range.classes.Range;
 
 import java.util.Arrays;
 
 public class RangeRunner {
     public static void main(String[] args) {
         Range range1 = new Range(1, 10);
-        Range range2 = new Range(2,5);
+        Range range2 = new Range(9, 11);
+        double point = 3;
+        System.out.println("Длина отрезка" + range1 + ": " + range1.getLength());
+        System.out.println("Принадлежит ли точка " + point + " к отрезку " + range1 + ":" + range1.isInside(point));
         System.out.println("Результат пересечения" + range1 + " и " + range2 + ": " + range1.getIntersection(range2));
         System.out.println("Результат объединения" + range1 + " и " + range2 + ": " + Arrays.toString(range1.getUnion(range2)));
         System.out.println("Разность " + range1 + " и " + range2 + ": " + Arrays.toString(range1.getDifference(range2)));
